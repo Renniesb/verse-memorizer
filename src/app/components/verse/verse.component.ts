5cac05ed2a91e2de8e5243459b2a1795288b56a3
Rennie Bevineau <rsbevineau@gmail.com>
2021-09-15 00:16:44 -0400

    add verse component

---
+++ src/app/components/verse/verse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verse',
+  templateUrl: './verse.component.html',
+  styleUrls: ['./verse.component.sass']
+})
+export class VerseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
